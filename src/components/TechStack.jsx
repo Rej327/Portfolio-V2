2add74526ffb2a78986c6fbfea79e24654cbddd6
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-03-19 09:57:08 +0800

    Update

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -3,9 +3,13 @@ import techstackmobile from "../assets/techstackmobile.png";
 
 export default function TechStack() {
   return (
-    <div className="w-full p-4">
-      <img src={techstack} className="w-full object-cover hidden md:block" />
-      <img src={techstackmobile} className="w-full object-cover md:hidden" />
+    <div className="w-full  ">
+      {/* <img src={techstack} className="w-full object-cover hidden md:block" />
+      <img src={techstackmobile} className="w-full object-cover md:hidden" /> */}
+      <div className="sm:px-44 object-fit">
+        <div className="desktop"></div>
+        <div className="mobile"></div>
+      </div>
     </div>
   );
 }
