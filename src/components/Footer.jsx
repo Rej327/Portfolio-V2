6843ea55dc80bf62bb70708eaaa12bc912ebc07f
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-03-19 10:24:26 +0800

    Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div className="w-full py-4 text-center text-white font-semibold bg-[#0E0A2D]">
-      <p className="">© 2023. ALL RIGHTS RESERVED | Created by Jefferson R.</p>
+    <div className="w-full py-4 text-center text-[#1d0a33] font-semibold bg-white">
+      <p className="">Created by Jefferson R.</p>
       <p className="text-sm">Create from Vite + React and TailwindCSS</p>
     </div>
   );
