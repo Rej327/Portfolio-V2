95ca272d920f3928cdffdd4a59f2605d1ea6b10c
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-03-19 09:41:20 +0800

    Add typings effect in cover letter

--- src/components/CoverLetter.jsx
+++ src/components/CoverLetter.jsx
@@ -1,15 +1,17 @@
 import writting from "../assets/writting.png";
+import useTypeWriter from "react-typewriter-hook";
 
 export default function CoverLetter() {
+  const cover = useTypeWriter(
+    "I'm a Entry Level Frontend Developer. Although I have no professional experience as a Web Developer but I have experience creating websites through self-study.",
+    { speed: 5000 }
+  );
+
   return (
     <div className="full px-2 my-5">
       <img src={writting} className="mx-auto w-10 sm:w-20 sm:mt-10" />
       <div className="w-fit mx-auto border-l-2 mt-5 mb-2 md:border-l-4  border-slate-600 md:pl-10 pl-5">
-        <p className="max-w-[30rem] md:text-2xl py-5">
-          I'm a Entry Level Frontend Developer. Although I have no professional
-          experience as a Web Developer but I have experience creating websites
-          through self-study.
-        </p>
+        <p className="max-w-[30rem] md:text-2xl py-5">{cover}</p>
       </div>
       <div className="w-fit mx-auto">
         <a href="#works">
